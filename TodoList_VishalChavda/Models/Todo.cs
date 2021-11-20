ab9eafffaae709e94ece0a4eb4f15ee0f1b2ad33
vishal <vishalchavdaaugst@gmail.com>
2021-11-19 19:38:06 -0500

    Added Comments.
    Added comments in:
    1. Model Todo.cs
    2. Few Razor pages

--- TodoList_VishalChavda/Models/Todo.cs
+++ TodoList_VishalChavda/Models/Todo.cs
@@ -7,19 +7,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TodoList_VishalChavda.Models
 {
+    /*
+     * Creating simple POCO class.
+     * This class will act as a model and be used to create table in the database.
+     */
     public class Todo
     {
-        [Key]
+
+        [Key] /* Will generate primary key on TodoId column in table.*/
         public int TodoId { get; set; }
 
-        [StringLength(20, MinimumLength = 6)]
+        [Required] /* Title field is mandatory. It can't be empty*/
+        [StringLength(20, MinimumLength = 6)] /* TItle cannot be less than 6 chars and more than 20 chars. */
         public string Title { get; set; }
 
         public string Description { get; set; }
 
+        [Display(Name = "Done")]    /* On view page field will be created with label "Done" */
         public bool IsDone { get; set; } = false;
 
-        public DateTime CompletionDate { get; set; }
+        [Display(Name ="Completion Date")]  /* On view page field will be created with label "Completion Date" */
+        public DateTime? CompletionDate { get; set; } /*Copletetion date field is optional, it can be empty*/
 
 
     }
